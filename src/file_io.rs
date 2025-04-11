af4007a6e9e66fb1c6e6e71fc176b05de8302195
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-04-10 21:50:14 -0500

    change indentation from 4 to 2 in some files

--- src/file_io.rs
+++ src/file_io.rs
@@ -8,27 +8,27 @@ use serde_json;
 use crate::types::SkgNode;
 
 pub fn read_skgnode_from_path
-    <P: AsRef<Path>>
-    (file_path: P)
-     -> io::Result<SkgNode>
+  <P: AsRef<Path>>
+  (file_path: P)
+   -> io::Result<SkgNode>
 { let file_path = file_path.as_ref();
   let contents = fs::read_to_string(file_path)?;
   let mut skgnode: SkgNode = serde_json::from_str(&contents)
-      .map_err(
-          |e| io::Error::new(
-              io::ErrorKind::InvalidData, e.to_string()))?;
+  .map_err(
+    |e| io::Error::new(
+      io::ErrorKind::InvalidData, e.to_string()))?;
   skgnode.path = file_path.to_path_buf(); // not part of the JSON
   Ok(skgnode) }
 
 /// A line in the typedef of SkgNode prevents the field `path`
 /// from being part of the JSON representation.
 pub fn write_skgnode_to_path
-    <P: AsRef<Path>>
-    (skgnode: &SkgNode, file_path: P)
-     -> io::Result<()>
+  <P: AsRef<Path>>
+  (skgnode: &SkgNode, file_path: P)
+   -> io::Result<()>
 { let json_string = serde_json::to_string(skgnode)
-      .map_err(
-          |e| io::Error::new(
-              io::ErrorKind::InvalidData, e.to_string()))?;
+  .map_err(
+    |e| io::Error::new(
+      io::ErrorKind::InvalidData, e.to_string()))?;
   fs::write(file_path, json_string)?;
   Ok(()) }
--- src/types.rs
+++ src/types.rs
@@ -9,81 +9,83 @@ use std::path::PathBuf;
 pub struct ID(String);
 
 impl ID {
-    pub fn new<S: Into<String>>(s: S) -> Self {
-        ID(s.into()) }
-    pub fn as_str(&self) -> &str {
-        &self.0 } }
+  pub fn new<S: Into<String>>(s: S) -> Self {
+    ID(s.into()) }
+  pub fn as_str(&self) -> &str {
+    &self.0 } }
 
-impl Deref for ID { // Lets ID be used like a String in (more?) cases.
-    type Target = String;
-    fn deref(&self) -> &Self::Target {
-        &self.0 } }
+impl Deref for ID {
+  // Lets ID be used like a String in (more?) cases.
+  type Target = String;
+  fn deref(&self) -> &Self::Target {
+    &self.0 } }
 
 impl fmt::Display for ID {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0) } }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>)
+         -> fmt::Result {
+    write!(f, "{}", self.0) } }
 
 impl From<String> for ID {
-    fn from(s: String) -> Self {
-        ID(s) } }
+  fn from(s: String) -> Self {
+    ID(s) } }
 
 impl From<&str> for ID {
-    fn from(s: &str) -> Self {
-        ID(s.to_string()) } }
+  fn from(s: &str) -> Self {
+    ID(s.to_string()) } }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum SkgNodeProperty {
-    CommentsOn(ID), // Relevant to TypeDB
-    NoTantivyIndex, // Relevant to Tantivy
+  CommentsOn(ID), // Relevant to TypeDB
+  NoTantivyIndex, // Relevant to Tantivy
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SkgNode {
-    // Tantivy will receive some of this data,
-    // and TypeDB some other subset.
-    // At least one field, `unindexed_text`, is known to neither.
+  // Tantivy will receive some of this data,
+  // and TypeDB some other subset.
+  // At least one field, `unindexed_text`, is known to neither.
 
-    pub titles: Vec<String>,
-    pub ids: Vec<ID>, // Must be nonempty. Can be more than 1 because
-                      // nodes might be merged.
-    pub unindexed_text: String, // Unknown to both Tantivy & TypeDB
+  pub titles: Vec<String>,
+  pub ids: Vec<ID>, // Must be nonempty. Can be more than 1 because
+  // nodes might be merged.
+  pub unindexed_text: String, // Unknown to both Tantivy & TypeDB
 
-    // Each #[serde directive applies to the field after it.
-    // serde(default) says use the default value (for Vec it's [])
-    // if no value is provided when reading from disk.
+  // Each #[serde directive applies to the field after it.
+  // serde(default) says use the default value (for Vec it's [])
+  // if no value is provided when reading from disk.
 
-    #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub properties: Vec<SkgNodeProperty>,
-    #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub nodes_contained: Vec<ID>,
-    #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub nodes_subscribed: Vec<ID>,
-    #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub nodes_unsubscribed: Vec<ID>,
+  #[serde(default, skip_serializing_if = "Vec::is_empty")]
+  pub properties: Vec<SkgNodeProperty>,
+  #[serde(default, skip_serializing_if = "Vec::is_empty")]
+  pub nodes_contained: Vec<ID>,
+  #[serde(default, skip_serializing_if = "Vec::is_empty")]
+  pub nodes_subscribed: Vec<ID>,
+  #[serde(default, skip_serializing_if = "Vec::is_empty")]
+  pub nodes_unsubscribed: Vec<ID>,
 
-    #[serde(skip)]  // `path` is not represented in the JSON.
-    pub path: PathBuf,  // It is instead inferred from filepath.
+  #[serde(skip)]  // `path` is not represented in the JSON.
+  pub path: PathBuf,  // It is instead inferred from filepath.
 }
 
 pub fn skgnode_example() -> SkgNode
 { SkgNode {
-    titles: vec![
-        "This text gets indexed.".to_string(),
-        "Maybe searching other text could find this note.".to_string(),
-        "JSON escapes \"quotation marks\" in text.".to_string() ],
+  titles: vec![
+    "This text gets indexed.".to_string(),
+    "Maybe searching other text could find this note.".to_string(),
+    "JSON escapes \"quotation marks\" in text.".to_string() ],
 
-    ids: vec![ ID::new("123") ],
-    unindexed_text: "this one string could span pages".to_string(),
-    properties: vec![
-        SkgNodeProperty::CommentsOn(ID::new("42")),
-    ],
-    nodes_contained: vec![ID::new("1"),
-                          ID::new("2"),
-                          ID::new("3")],
-    nodes_subscribed: vec![ID::new("11"),
-                           ID::new("12"),
-                           ID::new("13")],
-    nodes_unsubscribed: vec![],
-    path: PathBuf::from(
-        "tests/file_io/generated/example.skg"),
+  ids: vec![ ID::new("123") ],
+  unindexed_text: "this one string could span pages".to_string(),
+  properties: vec![
+    SkgNodeProperty::CommentsOn(ID::new("42")),
+  ],
+  nodes_contained: vec![ID::new("1"),
+                        ID::new("2"),
+                        ID::new("3")],
+  nodes_subscribed: vec![ID::new("11"),
+                         ID::new("12"),
+                         ID::new("13")],
+  nodes_unsubscribed: vec![],
+  path: PathBuf::from(
+    "tests/file_io/generated/example.skg"),
 } }
