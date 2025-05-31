386c7ec55abf417a3bfbd67b9d3694ba1996da71
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-05-31 12:50:21 -0500

    reorder some code
      to put callers first

--- rust/index_titles.rs
+++ rust/index_titles.rs
@@ -13,6 +13,61 @@ use serde_yaml::from_str;
 
 use crate::hyperlinks::strip_org_hyperlinks;
 
+pub fn search_index(
+  index: &Index,
+  title_field: schema::Field,
+  query_text: &str
+) -> Result< (Vec<(f32, tantivy::DocAddress)>,
+              tantivy::Searcher),
+              Box<dyn std::error::Error>> {
+  println!(
+    "\nFinding files with titles matching \"{}\".",
+    query_text);
+  let reader = index.reader()?;
+  let searcher = reader.searcher();
+  let query_parser =
+    tantivy::query::QueryParser::for_index(
+      &index, vec![title_field]);
+  let query = query_parser.parse_query(query_text)?;
+  let best_matches = searcher.search(
+    &query, &TopDocs::with_limit(10))?;
+  Ok((best_matches, searcher)) }
+
+pub fn update_index(
+  index: &Index,
+  path_field: schema::Field,
+  title_field: schema::Field,
+  data_dir: &str,
+  index_path: &Path
+) -> Result<usize, Box<dyn std::error::Error>> {
+  println!("Updating index with .skg files from {}.",
+           data_dir);
+  let mut index_writer = index.writer(50_000_000)?;
+  let mut indexed_count = 0;
+  let index_mtime = get_modification_time(index_path)
+    .unwrap_or(SystemTime::UNIX_EPOCH);
+  for entry in WalkDir::new(data_dir)
+    .into_iter().filter_map(Result::ok) {
+      let path = entry.path();
+      if !needs_indexing(path, index_mtime) {
+        continue; } // skip this file
+      if let Some(title) = extract_skg_title(path) {
+        index_title_at_path(
+          &mut index_writer,
+          path,
+          &title,
+          path_field,
+          title_field)?;
+        indexed_count += 1;
+      } }
+
+  if indexed_count > 0 {
+    println!("Indexed {} files. Committing changes...", indexed_count);
+    index_writer.commit()?;
+  } else {
+    println!("No new or modified files found."); }
+  Ok (indexed_count) }
+
 pub fn get_or_create_index(
   schema: schema::Schema,
   index_path: &Path
@@ -36,26 +91,6 @@ pub fn get_or_create_index(
     println!("Creating new index at {:?}", index_path);
     fs::create_dir_all(index_path)?;
     Ok(Index::create_in_dir(index_path, schema)?) } }
-
-pub fn get_modification_time(
-  path: &Path)
-  -> Result<SystemTime, Box<dyn std::error::Error>> {
-  let metadata = fs::metadata(path)?;
-  Ok(metadata.modified()?) }
-
-pub fn needs_indexing( // based on modification time
-  path: &Path,
-  index_mtime: SystemTime
-) -> bool {
-  if path.to_string_lossy().contains("index.tantivy") {
-    return false; } // Skip files in the index directory
-  if !path.extension().map_or( false, |ext| ext == "skg" ) {
-    return false; } // Skip non-skg files
-  match get_modification_time(path) {
-    Ok(file_mtime) => file_mtime > index_mtime,
-    Err(_) => true // If its modification time is unknown,
-                   // assume it needs indexing.
-  } }
 
 pub fn extract_skg_title(
   // Gets the title from the file at path,
@@ -113,57 +148,22 @@ pub fn index_title_at_path (
     writer, path, title, path_field, title_field)?;
   Ok (( )) }
 
-pub fn update_index(
-  index: &Index,
-  path_field: schema::Field,
-  title_field: schema::Field,
-  data_dir: &str,
-  index_path: &Path
-) -> Result<usize, Box<dyn std::error::Error>> {
-  println!("Updating index with .skg files from {}.",
-           data_dir);
-  let mut index_writer = index.writer(50_000_000)?;
-  let mut indexed_count = 0;
-  let index_mtime = get_modification_time(index_path)
-    .unwrap_or(SystemTime::UNIX_EPOCH);
-  for entry in WalkDir::new(data_dir)
-    .into_iter().filter_map(Result::ok) {
-      let path = entry.path();
-      if !needs_indexing(path, index_mtime) {
-        continue; } // skip this file
-      if let Some(title) = extract_skg_title(path) {
-        index_title_at_path(
-          &mut index_writer,
-          path,
-          &title,
-          path_field,
-          title_field)?;
-        indexed_count += 1;
-      } }
+pub fn needs_indexing( // based on modification time
+  path: &Path,
+  index_mtime: SystemTime
+) -> bool {
+  if path.to_string_lossy().contains("index.tantivy") {
+    return false; } // Skip files in the index directory
+  if !path.extension().map_or( false, |ext| ext == "skg" ) {
+    return false; } // Skip non-skg files
+  match get_modification_time(path) {
+    Ok(file_mtime) => file_mtime > index_mtime,
+    Err(_) => true // If its modification time is unknown,
+                   // assume it needs indexing.
+  } }
 
-  if indexed_count > 0 {
-    println!("Indexed {} files. Committing changes...", indexed_count);
-    index_writer.commit()?;
-  } else {
-    println!("No new or modified files found."); }
-  Ok (indexed_count) }
-
-pub fn search_index(
-  index: &Index,
-  title_field: schema::Field,
-  query_text: &str
-) -> Result< (Vec<(f32, tantivy::DocAddress)>,
-              tantivy::Searcher),
-              Box<dyn std::error::Error>> {
-  println!(
-    "\nFinding files with titles matching \"{}\".",
-    query_text);
-  let reader = index.reader()?;
-  let searcher = reader.searcher();
-  let query_parser =
-    tantivy::query::QueryParser::for_index(
-      &index, vec![title_field]);
-  let query = query_parser.parse_query(query_text)?;
-  let best_matches = searcher.search(
-    &query, &TopDocs::with_limit(10))?;
-  Ok((best_matches, searcher)) }
+pub fn get_modification_time(
+  path: &Path)
+  -> Result<SystemTime, Box<dyn std::error::Error>> {
+  let metadata = fs::metadata(path)?;
+  Ok(metadata.modified()?) }
