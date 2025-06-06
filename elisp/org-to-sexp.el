f043bb5e7042fa36916eb1e867abad79ab53c4c9
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-06-06 16:39:42 -0500

    comments

--- elisp/org-to-sexp.el
+++ elisp/org-to-sexp.el
@@ -3,6 +3,7 @@
   "RETURNS the value (which can be of any type)
 of the PROPERTY-NAME property on this line,
 or nil if there is none."
+
   (save-excursion
     (let ( (found-pos nil) )
       (setq found-pos (text-property-not-all
@@ -42,6 +43,7 @@ REQUIRES point to be on a heading; else throws an error."
 of the form (body . STRING).
 ASSUMES point is on the first line of a heading body.
 MOVES POINT to the first line after the body."
+
   (beginning-of-line)
   (let ((body-start (point))
         (body-end
@@ -58,18 +60,17 @@ MOVES POINT to the first line after the body."
 
 (defun org-to-sexp-parse-heading-at-point-and-maybe-body
     (&optional focused-line-number)
-  "Parses the heading at point and its body text if any.
-
+  "PURPOSE: Parses the heading at point and its body text if any.
 RETURNS an alist with these keys (some optional):
   heading  : string,
   ?id      : string,
   ?body    : string,
   ?focused : t or absent.
 ASSUMES point is on a heading.
-SIDE EFFECTS: Moves point to the line just after the parsed content.
+SIDE EFFECTS:
+  - Moves point to the line just after the parsed content.
+  - If `focused-line-number` is given and lies within the parsed region, the returned alist will include the pair (focused . t)."
 
-If `focused-line-number` is given and lies within the parsed region,
-the returned alist will include the pair (focused . t)."
   (let* ((start-line (line-number-at-pos))
          (heading-data (org-to-sexp-parse-heading-at-point))
          (body-sexp nil)
@@ -88,17 +89,16 @@ the returned alist will include the pair (focused . t)."
 
 (defun org-to-sexp-parse-branch
     (&optional focused-line-number)
-  "Parse the current heading, its body,
-and (recursively) any child branches.
-
+  "PURPOSE: Parse the current heading, its body,
+  and (recursively) any child branches.
 RETURNS an alist with these keys (some optional):
   heading  : string,
   ?id      : string,
   ?body    : string,
   ?focused : t or absent,
   ?content : list of alists.
-
 ASSUMES point is on a heading."
+
   (let* ((initial-level (org-outline-level))
          (node-data
           (org-to-sexp-parse-heading-at-point-and-maybe-body
@@ -126,14 +126,13 @@ ASSUMES point is on a heading."
     node-data))
 
 (defun org-to-sexp-parse-all-branches ()
-  "Returns an s-exp of the form `(content . results)`, where `results` is a list containing the result of calling `org-to-sexp-parse-branch`.
-
+  "PURPOSE: Returns an s-exp of the form `(content . results)`, where `results` is a list containing the result of calling `org-to-sexp-parse-branch`.
 RETURNS: a single cons cell:
   (content . LIST-OF-ALISTS),
   where the cdr is a list of the kind of thing
   returned by `org-to-sexp-parse-branch`.
-
 PITFALL: The s-expression might look malformed, because there's no (.) shown between `content` and `results`. But that's just a quirk of how Emacs displays a cons cell when the cdr is a list."
+
   (save-excursion
     (let ((focused-line-number (line-number-at-pos)))
       (progn ;; go to the first heading
