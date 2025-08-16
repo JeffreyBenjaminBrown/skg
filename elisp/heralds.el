d3636e97b89f38e11ac36d27c07467915b0aa1a6
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-08-16 12:59:42 -0500

    performance improvement
      When the user edits the buffer, redisplay heralds only for the affected lines.
    
      But when if external program changes the file, redisplay the entire buffer.

--- elisp/heralds.el
+++ elisp/heralds.el
@@ -6,7 +6,7 @@
 
 ;;;###autoload
 (define-minor-mode heralds-mode
-"When Rust sends a view of the graph to Emacs,
+  "When Rust sends a view of the graph to Emacs,
 each line (after the org-bullet) begins with some metadata.
 The API is in flux -- see api.md -- but might look something like
 <<id:long-string, repeated, key:value, another-value>>
@@ -23,19 +23,25 @@ The metadata is otherwise not displayed."
   (if heralds-mode
       (progn
         (heralds-apply-to-buffer)
-        (add-hook 'after-change-functions #'heralds-after-change nil t))
+        (add-hook ;; When a user edits some lines, redisplay heralds only for those lines.
+         'after-change-functions
+         #'heralds-after-change nil t)
+        (add-hook ;; In this case, re-render the entire file. (This might never happen, since a skg view corresponds to no file on disk.)
+         'after-revert-hook
+         #'heralds-apply-to-buffer nil t))
     (remove-hook 'after-change-functions #'heralds-after-change t)
+    (remove-hook 'after-revert-hook #'heralds-apply-to-buffer t)
     (heralds-clear-overlays)))
 
 (defvar-local heralds-overlays nil
   "List of overlays created by `heralds-mode'.")
 
 (defun heralds-apply-to-buffer ()
-  "Do `heralds-apply-on-line` to each line."
+  "Do `heralds-apply-to-line` to each line."
   (save-excursion
     (goto-char (point-min))
     (while (< (point) (point-max))
-      (heralds-apply-on-line)
+      (heralds-apply-to-line)
       (forward-line 1))))
 
 (defun heralds-apply-to-line ()
@@ -43,18 +49,18 @@ The metadata is otherwise not displayed."
 Creates one overlay (at most) and pushes it onto `heralds-overlays`."
   (save-excursion
     (let ((bol (line-beginning-position))
-          (eol (line-end-position)))
+          (eol (line-end-position)) )
       (goto-char bol)
       (when (re-search-forward "<<\\([^<>]*\\)>>" eol t)
         (let* ((beg (match-beginning 0))
                (end (match-end 0))
                (inner (match-string-no-properties 1))
-               (glyphs (heralds-from-metadata inner)))
-          (when glyphs
-            (let ((ov (make-overlay beg end)))
-              (overlay-put ov 'display glyphs)
+               (heralds (heralds-from-metadata inner)) )
+          (when heralds
+            (let ((ov (make-overlay beg end)) )
+              (overlay-put ov 'display heralds)
               (overlay-put ov 'evaporate t)
-              (push ov heralds-overlays))))))))
+              (push ov heralds-overlays)) )) )) ))
 
 (defun heralds-from-metadata
     (metadata) ;; line's first text inside (not including) << and >>
@@ -93,11 +99,29 @@ Whitespace in METADATA is ignored."
   (mapc #'delete-overlay heralds-overlays)
   (setq heralds-overlays nil))
 
-(defun heralds-after-change (&rest _)
-  "Refresh overlays after any buffer change."
+(defun heralds-clear-overlays-in-region (start end)
+  "Delete overlays we manage that overlap [START, END)."
+  (let (keep)
+    (dolist (ov heralds-overlays)
+      (if (and (< (overlay-start ov) end)
+               (> (overlay-end   ov) start))
+          (delete-overlay ov)
+        (push ov keep)))
+    (setq heralds-overlays (nreverse keep)) ))
+
+(defun heralds-after-change (beg end _len)
+  "Refresh overlays only on lines touched by the edit from BEG to END."
   (when heralds-mode
-    (heralds-clear-overlays)
-    (heralds-apply-to-buffer)))
+    (save-excursion
+      (let* ((lbeg (progn (goto-char beg) (line-beginning-position)) )
+             (lend (progn (goto-char end) (line-end-position)) )
+             (start-line (line-number-at-pos lbeg))
+             (end-line   (line-number-at-pos lend)) )
+        (heralds-clear-overlays-in-region lbeg lend)
+        (goto-char lbeg)
+        (dotimes (_ (1+ (- end-line start-line)) )
+          (heralds-apply-to-line)
+          (forward-line 1)) )) ))
 
 (defface heralds-blue-face
   '((t :foreground "white" :background "blue"))
@@ -120,4 +144,4 @@ Whitespace in METADATA is ignored."
 ;; Colors only: <<blue:Azure,green:Forest>>
 ;; <<foo:Azure,bar:Forest,bazoo>> Metadata with unrecognized keys and values is not rendered at all.
 
-(provide 'lensed-glyph)
+(provide 'heralds)
