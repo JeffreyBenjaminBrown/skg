1b359b7c74e71077f887b46fa7c57cf3399a68fa
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-09-20 00:33:46 -0500

    nothing (formatting, comments)

--- rust/render/containerward_view.rs
+++ rust/render/containerward_view.rs
@@ -1,14 +1,17 @@
 use crate::file_io::{read_node, path_from_pid};
+use crate::render::util::{
+  newline_to_space,
+  org_bullet,
+  get_node_title};
 use crate::typedb::search::{
   containerward_path,
   pid_from_id, };
 use crate::types::{ID, SkgConfig, SkgNode};
-use crate::render::util::{newline_to_space, org_bullet, get_node_title};
 
 use std::collections::HashSet;
 use std::error::Error;
-use typedb_driver::TypeDBDriver;
 use std::fmt::Write as _; // for the write! macro
+use typedb_driver::TypeDBDriver;
 
 /// Unlike single-root content view, which generates an entire buffer,
 /// this generates some new text that gets inserted into the buffer.
@@ -39,15 +42,14 @@ pub async fn containerward_org_view (
       let mut parts = vec![format!("id:{}", node_id)];
       if cycle_node.as_ref() == Some(node_id) {
         // The cycle node needs extra metadata.
-        parts.push("cycle".to_string());
-      }
+        parts . push ("cycle" . to_string () ); }
       if i > 0 { // The 'containsOrgParent' type.
         // All nodes except the terminus (first in path) get this type.
         // PITFALL: If there is a second appearance of the terminus,
         // later in the containerward path,
         // that appearance *should* (and does) get this type.
-        parts.push("type:containsOrgParent".to_string()); }
-      parts.join(",") };
+        parts . push ("type:containsOrgParent" . to_string () ); }
+      parts . join (",") };
     write! (
       & mut result_acc,
       "{} <skg<{}>> {}\n",
@@ -55,19 +57,19 @@ pub async fn containerward_org_view (
       metadata,
       newline_to_space (& node_title) )?;
     if i == 0 {
-      // If it exists, render the body
-      // of *only* the terminus (first in path) node.
+      // If it exists, render the body of *only*
+      // the terminus (first in path) node.
       let path = path_from_pid (
         config,
         pid_from_id (
           & config.db_name, driver, node_id
         ). await ? );
       let terminus_node : SkgNode = read_node (path) ?;
-      if let Some (ref body) = terminus_node.body {
-        result_acc.push_str(body);
-        if !body.ends_with('\n') {
-          result_acc.push('\n'); }} }}
-  let branch_or_cycle_level : usize = // The last level, if there are branches and/or a cycle. Otherwise, one more than the last level.
+      if let Some (ref body) = terminus_node . body {
+        result_acc . push_str (body);
+        if ! body.ends_with ('\n') {
+          result_acc.push ('\n'); }} }}
+  let branch_or_cycle_level : usize = // The last level, if there are branches and/or a cycle. Otherwise it is unused (and the last level is one less than this).
     terminus_level + path.len();
   if ! branches.is_empty () {
     // There are branches, one of which might be a cycle.
@@ -76,12 +78,10 @@ pub async fn containerward_org_view (
         driver, config, branch_id ). await ?;
       let metadata: String = {
         let mut parts = vec![format!("id:{}", branch_id)];
-        if cycle_node.as_ref() == Some (branch_id) {
-          parts.push("cycle".to_string());
-        }
-        parts.push("type:containsOrgParent".to_string());
-        parts.join(",")
-      };
+        if cycle_node . as_ref () == Some (branch_id) {
+          parts . push ("cycle" . to_string () ); }
+        parts . push ("type:containsOrgParent" . to_string() );
+        parts . join(",") };
       write! (
         &mut result_acc,
         "{} <skg<{}>> {}\n",
@@ -90,15 +90,14 @@ pub async fn containerward_org_view (
         newline_to_space (&node_title)
       )?; }}
   else if let Some (ref cycle_id) = cycle_node {
-    // There's a cycle but no branches.
+    // There's a cycle and no branches.
     let node_title: String = get_node_title (
       driver, config, cycle_id ). await ?;
     let metadata : String = {
       let mut parts = vec![
         format!("id:{}", cycle_id), "cycle".to_string() ];
       parts.push("type:containsOrgParent".to_string());
-      parts.join(",")
-    };
+      parts.join(",") };
     write! (
       &mut result_acc,
       "{} <skg<{}>> {}\n",
