b3676a032f9308915b0e9e57b40f4cf0aa28eaa1
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-09-24 21:28:37 -0500

    nothing
      comments, plans

--- rust/render/aliases.rs
+++ rust/render/aliases.rs
@@ -1,9 +1,22 @@
 use crate::types::{MetadataItem, OrgNodeType};
 
+/// Returns not an entire org buffer,
+/// but rather just a passage,
+/// which should replace the current headline and its body.
+/// The new passage include the current headline
+/// with potentially different metadata (if it is part of a cycle),
+/// the same body, a child 'aliases' headline,
+/// and a grandchild under it corresponding to each alias.
+/// If there are no aliases, the child will still be included --
+/// that's how a user can add aliases when there are none.
+/// Neither child nor grandchildren will include bodies.
+/// Prior children are retained
+/// (not that this function knows about them).
 pub fn aliases_to_org (
   aliases : Vec<String>,
   level   : usize,
 ) -> String {
+
   let header_level : usize = level + 1;
   let alias_level  : usize = level + 2;
   let aliases_metadata = MetadataItem::Type(
