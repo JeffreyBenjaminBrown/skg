c9966d5c5fc1dd5f6e5e226f0f4a8509ea8bd817
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2026-01-25 11:46:06 -0500

    nothing
      docs and scripts but no code

---
+++ ralph/README.md
@@ -0,0 +1,11 @@
+# This might not be needed
+
+Claude Code now has 'plan mode', where it builds a plan at
+'.claude/plans/' (with a nonsensicaly but almost-certainly-unique name)
+and then executes it.
+
+# What this is
+
+"Ralph" is a technique for repeatedly hammering at a task with Claude using the same spec, described (among other places) here:
+
+https://www.aihero.dev/getting-started-with-ralph
---
+++ ralph/repeated.sh
@@ -0,0 +1,24 @@
+# USAGE: First argument: request file.
+# Second argument: max number of iterations.
+
+set -e
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <request-file> <max-iterations>"
+  exit 1
+fi
+
+for ((i=1; i<=$2; i++)); do
+  result=$(claude --dangerously-skip-permissions \
+                  -p "$(cat $1)")
+
+  echo "$result"
+  echo "* iteration $i" >> iterations.org
+  echo "$result" | sed 's/^/  /' >> iterations.org
+
+  if [[ "$result" == *"<promise>COMPLETE</promise>"* ]]; then
+    echo "PRD complete after $i iterations."
+    aplay /tmp/gnarly.wav 2>/dev/null &
+    exit 0
+  fi
+done
