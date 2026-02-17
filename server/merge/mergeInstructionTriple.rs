44aa5d05bfa7f0e15f0de3c093834b19847aab3d
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2026-02-17 11:39:57 -0500

    minor refactor
      use an optional rather than a vector

--- server/merge/mergeInstructionTriple.rs
+++ server/merge/mergeInstructionTriple.rs
@@ -2,7 +2,7 @@ use crate::dbs::filesystem::one_node::skgnode_from_id;
 use crate::types::save::{Merge, SaveNode, DeleteNode};
 use crate::types::misc::{SkgConfig, ID};
 use crate::types::viewnode::EditRequest;
-use crate::types::viewnode::{ViewNode, ViewNodeKind};
+use crate::types::viewnode::{ViewNode, ViewNodeKind, TrueNode};
 use crate::types::skgnode::SkgNode;
 use crate::types::list::dedup_vector;
 use crate::util::setlike_vector_subtraction;
@@ -10,14 +10,12 @@ use ego_tree::Tree;
 use std::error::Error;
 use typedb_driver::TypeDBDriver;
 
-/// PURPOSE: For each ViewNode with a merge instruction,
-/// this creates a Merge:
+/// PURPOSE: For each ViewNode with a merge instruction, creates a Merge:
 /// - acquiree_text_preserver: new node containing the acquiree's title and body
 /// - updated_acquirer: acquirer node with modified contents and extra IDs
 /// - acquiree_to_delete: acquiree marked for deletion
 ///
-/// TODO ? This is slightly inefficient. It would be faster to collect a list
-/// of viewnodes with merge instructions during one of the other walks of the forest.
+/// TODO ? This is slightly inefficient, walking the forest yet again. It would be faster, but more complex, to collect a list of viewnodes with merge instructions during one of the other walks of the forest.
 pub async fn instructiontriples_from_the_merges_in_an_viewnode_forest(
   forest: &Tree<ViewNode>,
   config: &SkgConfig,
@@ -28,57 +26,50 @@ pub async fn instructiontriples_from_the_merges_in_an_viewnode_forest(
     Vec::new();
   for edge in forest.root().traverse() {
     if let ego_tree::iter::Edge::Open(node_ref) = edge {
-      merges.extend(
-        { let node_merges : Vec<Merge> =
-            merges_from_the_merge_in_an_viewnode(
-              { let viewnode: &ViewNode = node_ref.value();
-                viewnode },
-              config, driver ). await?;
-          node_merges } ); } }
+      if let Some(merge) =
+        optmerge_from_viewnode(
+          { let viewnode : &ViewNode = node_ref.value();
+            viewnode },
+          config, driver ). await?
+        { merges.push(merge); }} }
   Ok(merges) }
 
-/// PURPOSE: The name and type signature say it all.
-/// .
-/// TODO ? It's confusing that this returns a vector,
-/// because merges have a monogamy constraint:
-/// nothing can merge with more than one other node per save.
-/// Given that the metadata permits multiple '(merge _)' instructions,
-/// though, this is a natural way to write the function.
-async fn merges_from_the_merge_in_an_viewnode(
-  node: &ViewNode,
-  config: &SkgConfig,
-  driver: &TypeDBDriver,
-) -> Result<Vec<Merge>,
+/// Returns Some(Merge) if the viewnode has a merge instruction,
+/// None otherwise.
+async fn optmerge_from_viewnode (
+  node   : &ViewNode,
+  config : &SkgConfig,
+  driver : &TypeDBDriver
+) -> Result<Option<Merge>,
             Box<dyn Error>> {
-  let mut merge_instructions: Vec<Merge> =
-    Vec::new();
-  if let ViewNodeKind::True(t) = &node.kind {
-    if let Some(EditRequest::Merge(acquiree_id)) = &t.edit_request {
-      let acquirer_id : &ID = &t.id;
-      let acquirer_from_disk : SkgNode =
-        skgnode_from_id(
-          config, driver, acquirer_id ). await?;
-      let acquiree_from_disk : SkgNode =
-        skgnode_from_id(
-          config, driver, acquiree_id ) . await?;
-      let acquiree_text_preserver: SkgNode =
-        create_acquiree_text_preserver ( &acquiree_from_disk );
-      let updated_acquirer: SkgNode =
-        three_merged_skgnodes( &acquirer_from_disk,
-                               &acquiree_from_disk,
-                               &acquiree_text_preserver)?;
-      { merge_instructions.push(
-          Merge {
-            acquiree_text_preserver :
-              SaveNode(acquiree_text_preserver),
-            updated_acquirer :
-              SaveNode(updated_acquirer),
-            acquiree_to_delete :
-              DeleteNode {
-                id: acquiree_id.clone(),
-                source: acquiree_from_disk.source.clone() },
-          } ); }} }
-  Ok(merge_instructions) }
+  let t : &TrueNode = match &node . kind {
+    ViewNodeKind::True(t) => t,
+    _ => return Ok(None) };
+  let acquiree_id = match &t . edit_request {
+    Some(EditRequest::Merge(id)) => id,
+    _ => return Ok(None) };
+  let acquirer_id : &ID = &t . id;
+  let acquirer_from_disk : SkgNode =
+    skgnode_from_id(
+      config, driver, acquirer_id ). await?;
+  let acquiree_from_disk : SkgNode =
+    skgnode_from_id(
+      config, driver, &acquiree_id ). await?;
+  let acquiree_text_preserver : SkgNode =
+    create_acquiree_text_preserver ( &acquiree_from_disk );
+  let updated_acquirer : SkgNode =
+    three_merged_skgnodes( &acquirer_from_disk,
+                           &acquiree_from_disk,
+                           &acquiree_text_preserver)?;
+  Ok(Some(Merge {
+    acquiree_text_preserver :
+      SaveNode(acquiree_text_preserver),
+    updated_acquirer :
+      SaveNode(updated_acquirer),
+    acquiree_to_delete :
+      DeleteNode {
+        id     : acquiree_id . clone(),
+        source : acquiree_from_disk . source . clone() }} )) }
 
 /// Computes the updated acquirer node with all fields properly merged.
 /// Returns a new SkgNode with:
