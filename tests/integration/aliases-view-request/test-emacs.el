6d0dc4e44565e99da2fd8b92ce728d964f5527a7
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2026-04-09 13:04:49 -0500

    update some integration tests
      for recent metadata changes

--- tests/integration/aliases-view-request/test-emacs.el
+++ tests/integration/aliases-view-request/test-emacs.el
@@ -73,7 +73,7 @@ LINE-NUMBER is zero-based."
       (setq skg-port (string-to-number test-port))))
 
   (let ((expected-with-aliases
-         (concat "* (skg (node (id test-node) (source main) (graphStats (containers 0) (containsHerald 0{) aliasing))) Test Node\n"
+         (concat "* (skg (node (id test-node) (source main) (birth independent) (graphStats (containers 0) (containsHerald 0{) aliasing))) Test Node\n"
                  "** (skg aliasCol) its aliases\n"
                  "*** (skg alias) first alias\n"
                  "*** (skg alias) second alias\n"))
